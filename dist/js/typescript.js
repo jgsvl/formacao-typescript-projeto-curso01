3f0d3a3f60d31021823dcd80c57cd5aaee1e7bf3
joaogabriel.vidal <joaogabriel.vidal@eb.mil.br>
2026-04-10 16:24:58 -0300

    listas, type alias, enums

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -1,1 +1,24 @@
 "use strict";
+//tipos primitivos
+let valor = 3000.0;
+let nome = "joao";
+let isPago = true;
+let qualquerCoisa = "opa";
+qualquerCoisa = 45;
+// Arrays
+const listaAny = [];
+listaAny.push("joão", "Gabriel", 30, true, []);
+const listaNumeros = [];
+listaNumeros.push(9.45, 65, 30, .001, -4.4);
+// Enum
+var TipoTransacao;
+(function (TipoTransacao) {
+    TipoTransacao["DEPOSITO"] = "Dep\u00F3sito";
+    TipoTransacao["TRANSFERENCIA"] = "Transfer\u00EAncia";
+    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
+})(TipoTransacao || (TipoTransacao = {}));
+const novaTransacao = {
+    tipoTransacao: TipoTransacao.DEPOSITO,
+    data: new Date(),
+    valor: 0
+};
---
+++ src/typescript.ts
@@ -0,0 +1,37 @@
+//tipos primitivos
+
+let valor: number = 3000.0;
+let nome: string = "joao";
+let isPago: boolean = true;
+let qualquerCoisa: any = "opa";
+qualquerCoisa = 45;
+
+// Arrays
+
+const listaAny = [];
+listaAny.push("joão", "Gabriel", 30, true, []);
+
+const listaNumeros: number[] = [];
+listaNumeros.push(9.45, 65, 30, .001, -4.4);
+
+//Tipos personalizados (Type Alias)
+
+type Transacao = {
+    tipoTransacao: TipoTransacao,
+    data: Date,
+    valor: number
+}
+
+// Enum
+
+enum TipoTransacao {
+    DEPOSITO = "Depósito",
+    TRANSFERENCIA = "Transferência",
+    PAGAMENTO_BOLETO = "Pagamento de Boleto"
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: TipoTransacao.DEPOSITO,
+    data: new Date(),
+    valor: 0
+}
