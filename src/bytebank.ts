0821e4fea9e3c24483626675c1cfb0ea66377c5a
joaogabriel.vidal <joaogabriel.vidal@eb.mil.br>
2026-04-10 14:57:55 -0300

    refatorado para typescript

---
+++ src/bytebank.ts
@@ -0,0 +1,42 @@
+let saldo = 3000.00;
+
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+
+elementoSaldo.textContent = saldo.toString();
+
+const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
+elementoFormulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha todos os campos da transação!");
+        return;
+    }
+
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao") as HTMLSelectElement
+    const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement
+    const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement
+
+    let tipoTransacao: String = inputTipoTransacao.value;
+    let valor: number = inputValor.valueAsNumber;
+    let data: Date = new Date(inputData.value);
+
+    if (tipoTransacao == "Depósito"){
+        saldo += valor;
+    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto"){
+        saldo -= valor;
+    } else {
+        alert("Deu ruim");
+        return;
+    }
+
+    elementoSaldo.textContent = saldo.toString();
+    
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    }
+
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
